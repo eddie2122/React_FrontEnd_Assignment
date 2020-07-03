a70f9677c436b5c32c0c2a33eb262a9e59ffc81e
Eddie <edwinrichard2122@gmail.com>
2020-07-03 13:14:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import DataTable from './Components/DataTable';
+
+
+
+
 class App extends Component {
   constructor(props){
   super(props);
@@ -8,71 +12,71 @@ class App extends Component {
 
   let model = {
     headers: [
-      {title: "", accessor: "id", index: 0, width:10,  cell:{
+      {title: "", accessor: "id", index: 0, width:1,  cell:{
         type:"image",
         style:{
-          "width": "50px",
+          "width": "30px",
+          
         }}},
      
-      {title:"Item", accessor: "item", width: "10px", index: 2, },
+      {title:"Item", accessor: "item", width: "200px", index: 2, cell: row =>(
+        <div className="item">
+        <p>New Item</p>
+        </div>
+      )},
       {title:"", accessor : "chat_img", index: 3, cell:{
         type:"image",
         style:{
-          "width": "50px",
+          
+          "width": "30px",
         }}},
     
-      {title: "Person", accessor: "owner", width: "100px", index: 4, cell:{
+      {title: "Person", accessor: "owner", width: "50px", index: 4, cell:{
         type: "image",
         style: {
-          "width": "50px",
+          "width": "30px",
         }
       }},
-      {title: "Status", accessor: "status", index:5, width: "2000px", cell: row => (
-        <div className = "dropdown">
-          <div class="container">
-            <div class="dropdown">
-              <button type="button" class="btn btn-default dropdown-toggle"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 Status
-             </button>
+      {title: "Status", accessor: "status", index:5, width: "50px", cell: row => (
+        <div className = "button">
+          <select className="status" name="ststus" onchange="test(this);">
+            <option className="workingOnIt" value="workingOnIt">Working on it</option>
+            <option className="stuck" value="stuck">Stuck</option>
+            <option className="WaitingForReview" value="WaitingForReview">WaitingForReview</option>
+            <option className="green" value="Done">Done</option>
+            <option value="Blank2" className="option1"></option>
+            <option value="Blank1" className="option2"></option>
+            <option value="Critical" className="critical">Critical</option>
+          </select>
 
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="profile.com">Working on it</a>
-                    <a class="dropdown-item" href="profile.com">Stuck</a>
-                    <a class="dropdown-item" href="profile.com">Done</a>
-                    <a class="dropdown-item" href="profile.com">Waiting for review</a>
-                    <a class="dropdown-item" href="profile.com">Critical</a>
 
-                </div>
-            </div>
-          </div>
-              
-          {row}</div> 
-      )},
+        </div>)},
+
       {title: "", accessor: "completion_img", index:6, cell:  {
         type:"image",
         style:{
-          "width": "50px",
+          "width": "20px",
+          "align-items": "right",
         }}},
-      {title: "Due_date", accessor: "due_date", index:7},
+      {title: "Due_date", accessor: "due_date", index:7, width:"10px"},
       {title: "Priority", accessor: "priority", index:8},
-      {title: "button" , accessor:"button", index:9 }
     ],
     
+    
       data: [
-        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "profile.png", status: "", 
+        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "prefilenew1.png", status: "workingOnIt", 
         completion_img: "completion.png", due_date: "April 9", priority: "Urgent"},
-        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "profile.png", status: "", 
+        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "prefilenew1.png", status: "stuck", 
         completion_img: "completion.png", due_date: "April 10", priority: "High"},
-        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "profile.png", status: "", 
+        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "prefilenew1.png", status: "", 
         completion_img: "completion.png", due_date: "April 11", priority: "Medium"},
-        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "profile.png", status: "", 
+        {id: "id.bmp", item:"New Item", chat_img: "chat.png", owner: "prefilenew1.png", status: "", 
         completion_img: "completion.png", due_date: "April 12", priority: "Low"},
-        {id: "id.bmp", item:"mnhjbnkl", chat_img: "chat.png", owner: "profile.png", status: "", 
+        {id: "id.bmp", item:"mnhjbnkl", chat_img: "chat.png", owner: "prefilenew1.png", status: "", 
         completion_img: "completion.png", due_date: "April 13", priority: ""},
-        {id: "id.bmp", item:"Task2", chat_img: "chat.png", owner: "profile.png", status: "", 
+        {id: "id.bmp", item:"Task2", chat_img: "chat.png", owner: "prefilenew1.png", status: "", 
         completion_img: "completion.png", due_date: "April 14", priority: ""},
-
+        //add array
       ]
   }
   this.state = model;
--- src/Components/DataTable/index.js
+++ src/Components/DataTable/index.js
@@ -1,185 +1,277 @@
-import React from 'react';
-import './datatable.css';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import "./datatable.css";
+import ReactDOM from "react-dom";
 export default class DataTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headers: props.headers,
+      data: props.data,
+      rows: [{}] 
+    };
     
-    constructor(props) {
-        super(props);
+    this.keyField = props.keyField || "id";
+    this.noData = props.noData || "No records found!";
+    this.width = props.width || "100%";
+  }
+  
 
-        this.state = {
-                headers : props.headers,
-                data: props.data,
-                sortby: null,
-                decending: null
-          
-                
-        }
-        this.keyField = props.keyField || "id";
-        this.noData = props.noData || "No records found!";
-        this.width = props.width || "100%";
+  onDragOver = (e) => {
+    e.preventDefault();
+  };
 
-    }  
-     }    
-    onDragOver = (e) => {
-        e.preventDefault();
-    }
+  onDragStart = (e, source) => {
+    e.dataTransfer.setData("text/plain", source); //eBrowser
+  };
 
-    onDragStart = (e, source) => {
-        e.dataTransfer.setData('text/plain', source); //eBrowser
-    }
+  onDrop = (e, target) => {
+    e.preventDefault();
+    let source = e.dataTransfer.getData("text/plain");
+    let headers = [...this.state.headers];
+    let srcHeader = headers[source];
+    let targetHeader = headers[target];
 
-    onDrop = (e, target) => {
-        e.preventDefault();
-        let source = e.dataTransfer.getData('text/plain');
-        let headers = [...this.state.headers];
-        let srcHeader = headers[source];
-        let targetHeader = headers[target];
+    let temp = srcHeader.index; // swaping
+    srcHeader.index = targetHeader.index;
+    targetHeader.index = temp;
 
-        let temp =srcHeader.index;// swaping
-        srcHeader.index = targetHeader.index;
-        targetHeader.index = temp;
+    this.setState({
+      headers,
+    });
+  };
 
-        this.setState({
-            headers
-        });
-    }
+  renderTableHeader = () => {
+    let { headers } = this.state;
+    headers.sort((a, b) => {
+      if (a.index > b.index) return 1;
+      return -1;
+    });
+    
 
-    renderTableHeader = () => {
-        let {headers} = this.state;
-        headers.sort((a,b) => {
-            if (a.index > b.index) return 1;
-            return -1;
-        })
+    let headerView = headers.map((header, index) => {
+      let title = header.title;
+      let cleanTitle = header.accrssor;
+      let width = header.width;
 
-        let headerView = headers.map((header, index) => {
-            let title = header.title;
-            let cleanTitle = header.accrssor;
-            let width = header.width;
+      return (
+        <th
+          key={cleanTitle}
+          ref={(th) => (this.th = th)}
+          data-col={cleanTitle}
+          onDragStart={(e) => this.onDragStart(e, index)}
+          onDragOver={this.onDragOver}
+          onDrop={(e) => {
+            this.onDrop(e, index);
+          }}
+        >
+          <span draggable className="header-cell">
+            {title}
+          </span>
+        </th>
+      );
+    });
+    return headerView;
+  };
 
-            return (
-                <th key={cleanTitle}
-                ref={(th) => this.th =th}
-                data-col={cleanTitle}
-                onDragStart={(e) =>this.onDragStart(e, index)}
-                onDragOver={this.onDragOver}
-                onDrop={(e) =>{this.onDrop(e, index)}}>
-                        <span draggable className="header-cell">
-                            {title}
-                        </span>
-                </th>
-            )
-        })
-        return headerView;
-    }
+  renderNoData = () => {
+    return (
+      <tr>
+        <td colSpan={this.props.headers.length}>{this.noData}</td>
+      </tr>
+    );
+  };
 
-    renderNoData = () => {
-        return (
-            <tr>
-                <td colSpan={this.props.headers.length}>
-                    {this.noData}
-                </td>
-            </tr>
-        );
-    }
-
-    renderContent = () => {
-        
-        let {headers, data} = this.state;
-        let contentView = data.map((row, rowIdx) => {
-            let id = row[this.keyField];
-            let edit = this.state.edit;
-            let tds = headers.map((header, index) => {
-                let content = row[header.accessor];
-                let cell = header.cell;
-                if(cell){
-                    if(typeof(cell) === "object") {
-                        if (cell.type === "image" && content) {
-                            content = <img style={cell.style} src={content} alt=
-                            "temp" />
-                        } 
-                        }else if (typeof(cell) === "function") {
-                            content = cell(content);
-                    }
-                }
-
-                if (this.props.edit) {
-                    if (header.dataType && (header.dataType === "string"))
-                    if(edit && edit.row ===rowIdx && edit.cell === index) {
-                        content = (
-                            <form  onSubmit={this.onUpdate}>
-                                <input type="text" defaultValue={content}
-                                onKeyUp={this.onformReset}></input>
-                            </form>
-
-                        )
-                    }
-                }
-                return (
-
-                    
-                    <td key={index} data-id={id} data-row={rowIdx}>
-                        {content}
-
-                    </td>
-                );
-            });
-            return (
-                <tr key={rowIdx}> 
-                     {tds}
-                </tr>
-            );
-        });
-
-        return contentView;
-        
-    }
-
-    onSort = (e) => {
-        let data = this.state.data.sclice(); // give new array
-        let colIndex = ReactDOM.findDOMNODE(e.target).parentNode.cellIndex;
-
-        alert(colIndex);
-    }
-
-    onShowEditer = (e) => {
-        let item= e.target.dataset.id;
-        this.setState({
-            edit: {
-                row: parseInt(e.target.dataset.eow, 10),
-                cell: e.target.cellIndex
+  renderContent = () => {
+    let { headers, data } = this.state;
+    let contentView = data.map((row, rowIdx) => {
+      let id = row[this.keyField];
+      let edit = this.state.edit;
+      let tds = headers.map((header, index) => {
+        let content = row[header.accessor];
+        let cell = header.cell;
+        if (cell) {
+          if (typeof cell === "object") {
+            if (cell.type === "image" && content) {
+              content = <img style={cell.style} src={content} alt="temp" />;
             }
-        })
-    }
+          } else if (typeof cell === "function") {
+            content = cell(content);
+          }
+        }
 
-    renderTable =() =>{
-        let title = this.props.title || "DataTable";
-        let headerView =this.renderTableHeader();
-        let contentView = this.state.data.length > 0 ?
-                            this.renderContent() : 
-                            this.renderNoData();
-
+        if (this.props.edit) {
+          if (header.dataType && header.dataType === "string")
+            if (edit && edit.row === rowIdx && edit.cell === index) {
+              content = (
+                <form onSubmit={this.onUpdate}>
+                  <input
+                    type="text"
+                    defaultValue={content}
+                    onKeyUp={this.onformReset}
+                  ></input>
+                </form>
+              );
+            }
+        }
         return (
-            <table className="data-inner-table">
-                <caption className="data-table-caption">
-                    {title}
-                </caption>
-                <thead onclick={this.sort}>
-                    <tr>
-                        {headerView}
-                    </tr>
+          <td key={index} data-id={id} data-row={rowIdx}>
+            {content}
+          </td>
+        );
+      });
+      return <tr key={rowIdx}>{tds}</tr>;
+    });
+
+    return contentView;
+  };
+
+  onSort = (e) => {
+    let data = this.state.data.sclice(); // give new array
+    let colIndex = ReactDOM.findDOMNODE(e.target).parentNode.cellIndex;
+
+    alert(colIndex);
+  };
+
+  onShowEditer = (e) => {
+    let item = e.target.dataset.id;
+    this.setState({
+      edit: {
+        row: parseInt(e.target.dataset.eow, 10),
+        cell: e.target.cellIndex,
+      },
+    });
+  };
+  handleChange = idx => e => {
+      let data = this.state.data.slice();//Give New Array
+      const { name, value } = e.target;
+    const rows = [...this.state.rows];
+    rows[idx] = {
+      [name]: value
+    };
+    this.setState({
+      rows
+    });
+  };
+
+  handleAddRow = () => {
+    const item = {
+      name: "",
+      mobile: ""
+    };
+    this.setState({
+      rows: [...this.state.rows, item]
+    });
+  };
+  handleRemoveRow = () => {
+    this.setState({
+      rows: this.state.rows.slice(0, -1)
+    });
+  };
+  handleRemoveSpecificRow = (idx) => () => {
+    const rows = [...this.state.rows]
+    rows.splice(idx, 1)
+    this.setState({ rows })
+  }
+
+  
+
+  renderTable = () => {
+    let title = this.props.title || "DataTable";
+    let headerView = this.renderTableHeader();
+    let contentView =
+      this.state.data.length > 0 ? this.renderContent() : this.renderNoData();
+
+    return (
+      <table className="data-inner-table">
+        <caption className="data-table-caption">{title}</caption>
+        <thead onclick={this.sort}>
+          <tr>{headerView}</tr>
+        </thead>
+        <tbody >{contentView}
+        </tbody>
+        <button onclick={this.row}>Add</button>
+      </table>
+    );
+  };
+
+  render() {
+    return <div className={this.props.className}>
+      <div>
+      {this.renderTable()}
+        <div className="container">
+          <div className="row clearfix">
+            <div className="col-md-12 column">
+              <table
+                className="table table-bordered table-hover"
+                id="tab_logic"
+              >
+                <thead>
+                  <tr>
+                    <th className="text-center">  </th>
+                    <th className="text-center"> Item </th>
+                    <th className="text-center">  </th>
+                    <th className="text-center"> Owner </th>
+                    <th className="text-center"> Status </th>
+                    <th className="text-center">  </th>
+                    <th className="text-center">Due Date </th>
+                    <th className="text-center"> Priority </th>
+                    <th />
+                  </tr>
                 </thead>
-                <tbody onDoubleClick={this.onShowEditer}>
-                    {contentView}
+                <tbody>
+                  {this.state.rows.map((item, idx) => (
+                    <tr id="addr0" key={idx}>
+                      <td> <img src="id.bmp" alt="||" width="30"></img></td>
+                      <td>
+                        <input className="item1"
+                          type="text"
+                          name="name"
+                          value={this.state.rows[idx].name}
+                          onChange={this.handleChange(idx)}
+                          
+                        />
+                      </td>
+                      <td><div>
+                      <img src="chat.png" alt="||" width="30"></img>
+                      </div>
+                      </td>
+                      <td>
+                      <td>
+                      <div>
+                      <img src="profile.png" alt="||" width="40"></img>
+                      </div>
+                      </td>
+                      
+                      <td>
+                        <div>
+                          <p>Status</p>
+                        </div>
+                      </td>
+
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
-            </table>
-        )
-    }
-    render() {
-        return (
-            <div className={this.props.className}>
-                {this.renderTable()}
+              </table>
+              <button onClick={this.handleAddRow} className="btn btn-primary">
+                Add Row
+              </button>
+              <button
+                onClick={this.handleRemoveRow}
+                className="btn btn-danger float-right"
+              >
+                Delete Last Row
+              </button>
             </div>
-        )
-    }
+          </div>
+          </div>
+      </div>
+    
+  
+    
+    
+    </div>;
+  }
+  
+}
     
